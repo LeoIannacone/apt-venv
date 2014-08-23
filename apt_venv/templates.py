24884bc5a9d1d1b21f402661885e5094b6bebe0b
Leo Iannacone <l3on@ubuntu.com>
2014-08-23 13:23:22 +0200

    remove old templates

--- apt_venv/templates.py
+++ apt_venv/templates.py
@@ -1,26 +1,3 @@
-SOURCES_LIST = {}
-
-SOURCES_LIST['debian'] = """deb http://http.debian.net/debian %(distro)s main
-deb-src http://http.debian.net/debian %(distro)s main
-
-#deb http://http.debian.net/debian %(distro)s-updates main
-#deb-src http://http.debian.net/debian %(distro)s-updates main
-
-#deb http://security.debian.org/ %(distro)s/updates main
-#deb-src http://security.debian.org/ %(distro)s/updates main
-"""
-
-SOURCES_LIST['ubuntu'] = \
-"""deb http://archive.ubuntu.com/ubuntu/ %(distro)s main universe restricted multiverse
-deb-src http://archive.ubuntu.com/ubuntu/ %(distro)s main universe restricted multiverse
-
-deb http://security.ubuntu.com/ubuntu %(distro)s-security main universe restricted multiverse
-deb-src http://security.ubuntu.com/ubuntu %(distro)s-security main universe restricted multiverse
-
-deb http://archive.ubuntu.com/ubuntu/ %(distro)s-updates main universe restricted multiverse
-deb-src http://archive.ubuntu.com/ubuntu/ %(distro)s-updates main universe restricted multiverse
-"""
-
 BASHRC = \
 """source /etc/bash.bashrc
 source "${HOME}/.bashrc"
